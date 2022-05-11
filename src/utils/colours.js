7129caba5e76bf280006646e8ac712c9f5aadf84
Lachlan <lachlanrm@gmail.com>
2022-05-11 10:22:46 -0700

    Adds setup

---
+++ src/utils/colours.js
@@ -0,0 +1,32 @@
+export const JANE_BLUE = '#00c1ca'
+export const GREEN = '#8EC575'
+export const AMBER = '#FBA919'
+export const RED = '#DE4848'
+export const BUTTER = '#efd59a'
+export const VIOLET = '#c4c4d6'
+export const TANGERINE = '#f1b37a'
+export const DUSK = '#8eb8c9'
+export const MOSS = '#8EA785'
+export const POTPOURRI = '#D1A6C5'
+export const FLAMINGO = '#E8A2B6'
+export const MINT = '#A5D7C5'
+export const MAUVE = '#C38F92'
+export const GOSSIP = '#8EC576'
+export const GORSE = '#F2E042'
+export const DARK_TANGERINE = '#FBA91A'
+export const ROMAN = '#E06359'
+export const MANDARIAN_ORANGE = '#9B2219'
+export const GRAY = '#81818E'
+
+export const GRAPH_PRIMARY_BLUE = '#76D1D6'
+export const GRAPH_SECONDARY_BLUE = '#62BDC2'
+export const HIGHLIGHT_BLUE = '#B6E6E6'
+
+export const ANALYTICS_PRIMARY = '#abcba9' // rgb 171, 203, 169
+/* export const ANALYTICS_PRIMARY = MINT // rgb 171, 203, 169 */
+export const ANALYTICS_SECONDARY = GRAPH_SECONDARY_BLUE // rgb 0, 193, 202
+
+// TEXT COLORS
+export const TEXT = '#333333'
+export const TEXT_MUTED = '#696977'
+export const TEXT_DISABLED = '#81818E'
